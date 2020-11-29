dc8a381d5f2002b6fe82bde22e451c65632a54a4
Erin <erinpwoo@yahoo.com>
2020-11-28 20:44:00 -0800

    Added movement scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform truck;
+    public Vector3 offset;
+    public float followSpeed = 10f;
+    public float lookSpeed = 10f;
+
+    public void LookAtTarget()
+    {
+        Vector3 lookDirection = truck.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 targetPosition = truck.position + truck.forward * offset.z + truck.right * offset.x + truck.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+}
---
+++ Assets/Scripts/TruckController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour
+{
+
+    private float m_horizontalInput;
+    private float m_verticalInput;
+    private float m_steeringAngle;
+
+    public WheelCollider frontRightW, frontLeftW, trailer1_leftW, trailer1_rightW, trailer2_leftW, trailer2_rightW, trailer3_leftW, trailer3_rightW, trailer4_leftW, trailer4_rightW, trailer5_leftW, trailer5_rightW;
+    public Transform frontRightT, frontLeftT, trailer1_leftT, trailer1_rightT, trailer2_leftT, trailer2_rightT, trailer3_leftT, trailer3_rightT, trailer4_leftT, trailer4_rightT, trailer5_leftT, trailer5_rightT;
+
+    public float maxSteerAngle = 30; // in degrees
+    public float motorForce = 1000;
+
+
+    public void GetInput()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        m_steeringAngle = maxSteerAngle * m_horizontalInput;
+        frontLeftW.steerAngle = m_steeringAngle;
+        frontRightW.steerAngle = m_steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontLeftW.motorTorque = m_verticalInput * motorForce;
+        frontRightW.motorTorque = m_verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontRightW, frontRightT);
+        UpdateWheelPose(frontLeftW, frontLeftT);
+        UpdateWheelPose(trailer1_leftW, trailer1_leftT);
+        UpdateWheelPose(trailer1_rightW, trailer1_rightT);
+        UpdateWheelPose(trailer2_leftW, trailer2_leftT);
+        UpdateWheelPose(trailer2_rightW, trailer2_rightT);
+        UpdateWheelPose(trailer3_leftW, trailer3_leftT);
+        UpdateWheelPose(trailer3_rightW, trailer3_rightT);
+        UpdateWheelPose(trailer4_leftW, trailer4_leftT);
+        UpdateWheelPose(trailer4_rightW, trailer4_rightT);
+        UpdateWheelPose(trailer5_leftW, trailer5_leftT);
+        UpdateWheelPose(trailer5_rightW, trailer5_rightT);
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
