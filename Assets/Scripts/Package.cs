b3e853b493cb46d7c61c893afa42dc7363f506c1
Erin <erinpwoo@yahoo.com>
2020-11-29 22:42:04 -0800

    Added throw package + basic points UI

---
+++ Assets/Scripts/Package.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Package : MonoBehaviour
+{
+    public int pointValue;
+    public GameObject destination;
+    public Player player;
+    public float timeLimit;
+    public Text pointsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == destination)
+        {
+            player.points += pointValue;
+            pointsText.text = "Points: " + player.points;
+        }
+    }
+}
